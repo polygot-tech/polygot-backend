c31e34fd797027895ae77f8a7017e5955226adf0
Your Name <your_email@example.com>
2025-06-28 16:01:53 +0530

    FIX:CORS issue

--- index.ts
+++ index.ts
@@ -11,48 +11,38 @@ import keysRoutes from './routes/keys.routes'
 import userRoutes from './routes/user.routes'
 import './services/passport.service'; // Passport configuration
 
-// Load environment variables
 dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
-// --- Middleware ---
-
-// Enable CORS for the frontend application
 app.use(
   cors({
-    origin: '*',
+    origin: 'https://polygot-react.vercel.app',
     credentials: true, // Allow cookies to be sent
   })
 );
 
-// To parse cookies
 app.use(cookieParser());
 
-// To parse JSON bodies
 app.use(express.json());
 
-// Express session middleware
-// This is required for Passport's OAuth flow to work.
 app.use(
   session({
     secret: process.env.COOKIE_SECRET || 'default_secret',
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+      secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
     },
   })
 );
 
-// Initialize Passport and restore authentication state, if any, from the session.
 app.use(passport.initialize());
 app.use(passport.session());
 
 
-// --- Routes ---
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is running and ready to authenticate!');
@@ -64,7 +54,6 @@ app.use('/api/v1/keys', keysRoutes);
 app.use('/api/v1/user',userRoutes)
 
 
-// --- Server Initialization ---
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
