84ae9627f98e88d4cf2e60ad5c98a2309a44e2bc
godsplanhk <godsplanhk@gmail.com>
2025-07-07 10:04:20 +0530

    added originCheck middleware

--- routes/translate.routes.ts
+++ routes/translate.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { translate } from "../controllers/translate.controller";
-import { apiKeyCheck } from "../middleware/key.middleware";
+// import { apiKeyCheck } from "../middleware/key.middleware";
 import { originCheck } from "../middleware/originCheck.middleware";
 
 const router = Router();
 
-router.post("/", apiKeyCheck, translate);
+router.post("/", originCheck, translate);
 
 export default router;
